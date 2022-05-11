a3591139d718093ee65444373cfc46540d0736f4
Arnav Gupta <championswimmer@gmail.com>
2022-05-11 23:00:24 +0530

    initial project setup
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ src/main/java/com/scaler/blogapp/BlogappSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.blogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogappSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogappSpringbootApplication.class, args);
+    }
+
+}
